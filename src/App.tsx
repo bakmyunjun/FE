611f0f419352240c497f64dd9a9af30470db3519
goorm <goorm@goormui-MacBookPro.local>
2026-01-02 11:11:07 +0900

    init: 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex h-10 items-center rounded-md bg-white px-4">App</div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient.ts';
+import { StrictMode } from 'react';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    ,
+  </StrictMode>,
+);
