d77ee27aede4647eb7c7d296bb2cdc70de65334c
JIN921 <wlslwlsl808@gmail.com>
2026-01-02 23:46:12 +0900

    Setting: 디자인 토큰 생성

---
+++ theme/designToken.ts
@@ -0,0 +1,28 @@
+export const units = {
+  breakpoint: {
+    mobile: 360,
+    tablet: 768,
+    desktop: 1024,
+  },
+  spacing: {
+    none: 0,
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40,
+  },
+  padding: {
+    sm: 12,
+    md: 16,
+    mdPlus: 18,
+    lg: 20,
+  },
+  radius: {
+    sm: 8,
+    md: 12,
+    lg: 16,
+    full: 9999,
+  },
+} as const;
---
+++ theme/typography.ts
@@ -0,0 +1,85 @@
+const typography = {
+  H1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 700,
+    fontSize: '40px',
+    lineHeight: '48px',
+    letterSpacing: '-0.02em',
+  },
+  H2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 700,
+    fontSize: '32px',
+    lineHeight: '40px',
+    letterSpacing: '-0.02em',
+  },
+  H3: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '24px',
+    lineHeight: '30px',
+    letterSpacing: '-0.02em',
+  },
+
+  Sub1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '18px',
+    lineHeight: '22px',
+    letterSpacing: '-0.02em',
+  },
+  Sub2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '16px',
+    lineHeight: '20px',
+    letterSpacing: '-0.02em',
+  },
+
+  Body1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 500,
+    fontSize: '16px',
+    lineHeight: '24px',
+    letterSpacing: '-0.02em',
+  },
+  Body2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 500,
+    fontSize: '14px',
+    lineHeight: '20px',
+    letterSpacing: '-0.02em',
+  },
+
+  Caption1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '12px',
+    lineHeight: '16px',
+    letterSpacing: '-0.02em',
+  },
+  Caption2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 500,
+    fontSize: '12px',
+    lineHeight: '16px',
+    letterSpacing: '-0.02em',
+  },
+
+  Button1: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '16px',
+    lineHeight: '24px',
+    letterSpacing: '-0.02em',
+  },
+  Button2: {
+    fontFamily: 'Pretendard',
+    fontWeight: 600,
+    fontSize: '14px',
+    lineHeight: '20px',
+    letterSpacing: '-0.02em',
+  },
+};
+
+export default typography;
