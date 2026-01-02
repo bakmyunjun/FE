a5fd85d18c6a13bfafc530b1542acaaa86f00b2a
JIN921 <wlslwlsl808@gmail.com>
2026-01-03 00:11:57 +0900

    Setting: 폰트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex h-10 items-center rounded-md bg-white px-4">
-      <span className="font-normal">레귤러</span>
-      <span className="font-semibold">세미볼드</span>
-      <span className="font-bold">볼드</span>
+    <div className="flex h-10 items-center gap-4 rounded-md bg-white px-4 font-sans">
+      <span className="text-body1 font-medium">레귤러</span>
+      <span className="text-body1 font-semibold">세미볼드</span>
+      <span className="text-body1 font-bold">볼드</span>
     </div>
   );
 }
