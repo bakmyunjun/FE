6ad5c74f8525ebd517a071f56642871f881c6dfe
uzzin <01uzzin@gmail.com>
2026-01-02 22:59:57 +0900

    init: 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex h-10 items-center rounded-md bg-white px-4">App</div>
+    <div className="flex h-10 items-center rounded-md bg-white px-4">
+      <span className="font-normal">레귤러</span>
+      <span className="font-semibold">세미볼드</span>
+      <span className="font-bold">볼드</span>
+    </div>
   );
 }
 
