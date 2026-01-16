f9002e19b396cdd3a004671afb35e910d98a7b5e
JIN921 <wlslwlsl808@gmail.com>
2026-01-16 18:34:49 +0900

    HOTFIX: 라우팅 문제 해결

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,19 @@
 import { Button } from '@/components/ui/button';
 import logo from '@/assets/images/logo.svg';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const navi = useNavigate();
   return (
     <header className="text-h3 flex h-16 flex-row items-center justify-between border-b px-6 font-bold">
-      <div className="flex flex-row items-center justify-center gap-2">
+      <button
+        type="button"
+        onClick={() => navi('/')}
+        className="flex flex-row items-center justify-center gap-2"
+      >
         <img className="h-10 w-10" src={logo} alt="로고 이미지" />
         <div>bakmyunjun</div>
-      </div>
+      </button>
       <Button onClick={() => navi('/interview')}>면접 시작</Button>
     </header>
   );
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/layout/footer';
 import Header from '@/components/layout/header';
 import { Suspense } from 'react';
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
